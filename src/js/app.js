2e03da08d3cc9f8ca5bbfbb163e691da061b10f2
pavelklm <pavel.klm.verstka@gmail.com>
2023-08-28 20:08:29 +0300

    add header footer and 4 sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,20 @@
 import * as flsFunctions from "./modules/functions.js";
+import Swiper from 'swiper';
+import mixitup from 'mixitup';
 
 flsFunctions.isWebp();
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const swiper = new Swiper('.swiper', {
+    loop: false,
+    enabled: false,
+  });
+
+  const containerEl = document.querySelector('.images__container');
+
+  const mixer = mixitup(containerEl);
+});
+
+
