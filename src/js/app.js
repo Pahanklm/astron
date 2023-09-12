39f239cae44a70ec5a32707f54e6dfbcf5015085
pavelklm <pavel.klm.verstka@gmail.com>
2023-09-12 22:57:44 +0300

    add popup fixing form add send form

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ import mixitup from 'mixitup';
 import Choices from 'choices.js';
 import Accordion from 'accordion-js';
 
+
+
 flsFunctions.isWebp();
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -17,6 +19,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
   // swiper-end
+
+  iziToast.settings({
+    title: 'Дякуємо!',
+    message: "Невдовзі наш менеджер з вами зв'яжеться",
+    color: 'green', // Цвет фона уведомления
+    position: 'bottomRight', // Позиция на экране
+    timeout: 5000, // Время показа (в миллисекундах)
+    pprogressBarColor: 'rgb(173, 216, 230)', // Цвет полосы загрузки
+    // Другие опции и настройки
+  });
+
+  // izi-toast-end
+
+
+  const selectElement = document.getElementById('Select');
+  const choices = new Choices(selectElement, {
+    allowHTML: true,
+    searchEnabled: false,
+    itemSelectText: '',
+    shouldSort: false,
+  });
+
+  const selectElementModal = document.getElementById('Select-modal');
+  let choicesModal = new Choices(selectElementModal, {
+    allowHTML: true,
+    searchEnabled: false,
+    itemSelectText: '',
+    shouldSort: false,
+  });
+  // choices-end
 
   new Accordion('.accordion-container', {
   });
@@ -307,190 +339,301 @@ document.addEventListener("DOMContentLoaded", function () {
       acTitle.classList.toggle('active', !isExpanded);
     });
   });
+
+
+
+  const forms = document.querySelectorAll('.send__message-form'); // Выбираем обе формы
+
+  forms.forEach((form) => {
+    const nameInput = form.querySelector('.name-input');
+    const phoneInput = form.querySelector('.phone-input');
+    const textarea = form.querySelector('.send__message-textarea');
+    const radioButtons = form.querySelectorAll('input[name="image-radio"]');
+    const submitButton = form.querySelector('.send__message-btn');
+    const nameError = form.querySelector('.send__message-error#nameError');
+    const nameErrorlength = form.querySelector('.send__message-error#nameErrorlength');
+    const nameErrorEmpty = form.querySelector('.send__message-error#nameErrorEmpty');
+    const phoneError = form.querySelector('.send__message-error#phoneError');
+    const phoneErrorEmpty = form.querySelector('.send__message-error#phoneErrorEmpty');
+    let selectedRadio = form.querySelector('input[name="image-radio"]:checked');
+
+
+
+    function showErrorMessage(inputElement, errorMessage) {
+      inputElement.classList.add('error');
+      errorMessage.style.display = 'block';
+    }
+
+    function hideErrorMessage(inputElement, errorMessage) {
+      inputElement.classList.remove('error');
+      errorMessage.style.display = 'none';
+    }
+
+    phoneInput.addEventListener('mousedown', function (event) {
+      // Предотвращаем начало выделения текста при клике
+      event.preventDefault();
+    });
+
+    phoneInput.addEventListener('keydown', function (event) {
+      if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+        event.preventDefault();
+      }
+    });
+
+    phoneInput.addEventListener('click', function () {
+      placeCursorAtEnd(phoneInput);
+    });
+
+    function placeCursorAtEnd(input) {
+      input.focus(); // Устанавливаем фокус на поле ввода
+      const length = input.value.length; // Получаем длину значения в поле
+      input.setSelectionRange(length, length); // Устанавливаем курсор в конец строки
+    }
+
+    let lastValue = ''; // Сохраняем последнее введенное значение
+
+    function addPrefix() {
+      if (!phoneInput.value.startsWith('+38 (')) {
+        phoneInput.value = '+38 (';
+        phoneInput.setSelectionRange(6, 6); // Помещаем курсор после открывающей скобки
+      }
+    }
+
+    function formatePhone(event) {
+      const phoneLength = phoneInput.value.length;
+      let phoneValue = phoneInput.value;
+
+      phoneValue = phoneValue.replace(/[^0-9+ ()]/g, '');
+
+      if (phoneLength === 9 && event.key !== ' ') {
+        phoneValue = phoneValue.slice(0, 9) + ' ' + phoneValue.slice(9);
+      }
+
+      if (event.key === 'Backspace' && phoneLength === 10) {
+        phoneValue = phoneValue.slice(0, -2);
+      }
+      if (event.key === 'Backspace' && (phoneLength === 14 || phoneLength === 17)) {
+        phoneValue = phoneValue.slice(0, -1);
+      }
+      if (phoneLength === 5) {
+        if (event.key !== '0') {
+          event.preventDefault();
+        }
+      }
+      if (phoneLength === 8) {
+        phoneValue += ') ';
+      }
+      if (phoneLength === 13) {
+        phoneValue += ' ';
+      }
+      if (phoneLength === 16) {
+        phoneValue += ' ';
+      }
+
+      if (phoneLength > 19) {
+        phoneValue = phoneValue.slice(0, 19);
+      }
+
+      phoneInput.value = phoneValue;
+    }
+
+    function preventRemovalOfPrefix(event) {
+      const phoneValue = phoneInput.value;
+      const prefix = '+38 (';
+
+      if (!phoneValue.startsWith(prefix)) {
+        phoneInput.value = prefix;
+      }
+    }
+
+    phoneInput.addEventListener('input', preventRemovalOfPrefix);
+
+    phoneInput.addEventListener('keydown', formatePhone);
+
+    phoneInput.addEventListener('input', function (event) {
+      formatePhone(event); // Передаем объект события event
+      if (phoneInput.value.length < lastValue.length) {
+        // Пользователь удалил символ, не обновляем форматирование
+        lastValue = phoneInput.value;
+        return;
+      }
+
+      checkFields();
+    });
+
+    phoneInput.addEventListener('focus', addPrefix);
+
+    function checkFields() {
+      const nameValue = nameInput.value.trim();
+      const phoneValue = phoneInput.value.trim();
+      const noEnglish = /^[а-яА-ЯёЁіІїЇґҐ\s'-]+$/u.test(nameValue);
+      const nameLength = /^.{3,15}$/.test(nameValue);
+      const phoneValid = /^\+38 \(\d{3}\) \d{3} \d{2} \d{2}$/.test(phoneValue);
+
+      if (!nameValue) {
+        showErrorMessage(nameInput, nameErrorEmpty);
+      } else {
+        hideErrorMessage(nameInput, nameErrorEmpty);
+      }
+
+      if (!noEnglish && nameValue) {
+        showErrorMessage(nameInput, nameError);
+      } else {
+        hideErrorMessage(nameInput, nameError);
+      }
+      if (!nameLength && nameValue && noEnglish) {
+        showErrorMessage(nameInput, nameErrorlength);
+      } else {
+        hideErrorMessage(nameInput, nameErrorlength);
+      }
+
+      if (!phoneValue) {
+        showErrorMessage(phoneInput, phoneErrorEmpty);
+      } else {
+        hideErrorMessage(phoneInput, phoneErrorEmpty);
+      }
+      if (!phoneValid && phoneValue) {
+        showErrorMessage(phoneInput, phoneError);
+      } else {
+        hideErrorMessage(phoneInput, phoneError);
+      }
+
+      if (!nameValue || !phoneValue || !noEnglish || !nameLength || !phoneValid) {
+        submitButton.setAttribute('disabled', 'disabled');
+      } else {
+        submitButton.removeAttribute('disabled');
+      }
+    }
+
+    nameInput.addEventListener('input', checkFields);
+
+
+    radioButtons.forEach((radioButton) => {
+      radioButton.addEventListener('click', function () {
+        radioButtons.forEach((rb) => {
+          rb.checked = false;
+        });
+        this.checked = true;
+        selectedRadio = this; // Обновляем значение selectedRadio при клике
+      });
+    });
+
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      let selectedElement;
+      if (form.id === 'form1') {
+        selectedElement = choices.itemList.element.innerText
+      } else if (form.id === 'form2') {
+        selectedElement = choicesModal.itemList.element.innerText;
+      }
+
+
+
+      const botToken = '6405324996:AAHLBZFVoohzrqeSfC43aSFGdakbo2bkJQA';
+      const chatId = '1386471978'; // Замените на свой chat_id
+      const message = `
+      Нове замовлення
+
+      Ім'я: ${nameInput.value}
+      Телефон: ${phoneInput.value} 
+      Послуга: ${selectedElement}
+      Зв'язатися по: ${selectedRadio.value}
+      Коментар : ${textarea.value}`;
+
+      axios
+        .post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+          chat_id: chatId,
+          text: message,
+        })
+        .then((response) => {
+          nameInput.value = '';
+          phoneInput.value = '';
+          textarea.value = '';
+
+          modal.style.opacity = "0";
+          modal.classList.remove("modal-open");
+          modal.classList.add("modal-closed");
+          submitButton.setAttribute('disabled', 'disabled');
+          document.body.style.overflow = 'auto';
+          iziToast.show()
+
+        })
+        .catch((error) => {
+          modal.style.opacity = "0";
+          modal.classList.remove("modal-open");
+          modal.classList.add("modal-closed");
+          document.body.style.overflow = 'auto';
+          iziToast.error({
+            title: 'Ошибка',
+            message: 'Ошибка при отправке сообщения',
+            position: 'topCenter',
+            icon: 'fa fa-times',
+            timeout: 3000,
+            color: 'red',
+          })
+        });
+    });
+  });
+
+  const form2 = document.querySelector('#form2'); // Добавьте эту строку, чтобы получить доступ к форме с id="form2"
+  const modalButtons = document.querySelectorAll('.open-modal-button');
+  const modalButtonsWithContent = document.querySelectorAll('.open-modal-button-with-content');
+  const textarea = form2.querySelector('.send__message-textarea'); // Используйте form2 для получения textarea
+  const modal = document.getElementById("myModal");
+  modal.style.opacity = "0"; // Устанавливаем начальную прозрачность на 0
+
+  modalButtons.forEach(function (button) {
+    button.onclick = function () {
+      modal.style.opacity = "1"; // Устанавливаем прозрачность на 1, чтобы сделать его видимым
+      modal.classList.add("modal-open");
+      modal.classList.remove("modal-closed");
+      textarea.value = '';
+      document.body.style.overflow = 'hidden';
+    };
+  });
+
+  modalButtonsWithContent.forEach(function (button) {
+    button.onclick = function () {
+      modal.style.opacity = "1";
+      modal.classList.add("modal-open");
+      modal.classList.remove("modal-closed");
+      const textContent = this.firstElementChild.innerHTML.replace(/<br>/g, '').replace(/\s+/g, ' ').trim();
+      textarea.value = textContent;
+      const choicesDropdown = choices.dropdown.element;
+
+      const choicesItems = Array.from(choicesDropdown.children[0].children)
+        .map((item) => item.textContent);
+      console.log(choicesItems);
+
+      const controlItem = document.querySelector('.mixitup-control-active');
+      const controlItemText = controlItem.textContent.replace(/<br>/g, '').replace(/\s+/g, ' ').trim()
+      console.log(controlItemText);
+      if (choicesItems.includes(controlItemText)) {
+        choicesModal.itemList.element.innerText = controlItemText.trim();
+      } else {
+        console.log('Элемент не найден в списке choicesItems');
+      }
+      document.body.style.overflow = 'hidden';
+    };
+  });
+
+
+  document.querySelector(".close").onclick = function () {
+    modal.style.opacity = "0";
+    modal.classList.remove("modal-open");
+    modal.classList.add("modal-closed");
+    document.body.style.overflow = 'auto';
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.opacity = "0"; // Устанавливаем прозрачность на 0, чтобы спрятать его
+      modal.classList.remove("modal-open");
+      modal.classList.add("modal-closed");
+      document.body.style.overflow = 'auto';
+    }
+  };
 })
 
 
 
-const nameInput = document.getElementById('nameInput');
-const phoneInput = document.getElementById('phoneInput');
-const submitButton = document.getElementById('submitButton');
-const nameError = document.getElementById('nameError');
-const nameErrorlength = document.getElementById('nameErrorlength');
-const nameErrorEmpty = document.getElementById('nameErrorEmpty');
-const phoneError = document.getElementById('phoneError');
-const phoneErrorEmpty = document.getElementById('phoneErrorEmpty');
-
-function showErrorMessage(inputElement, errorMessage) {
-  inputElement.classList.add('error');
-  errorMessage.style.display = 'block';
-}
-
-function hideErrorMessage(inputElement, errorMessage) {
-  inputElement.classList.remove('error');
-  errorMessage.style.display = 'none';
-}
-phoneInput.addEventListener('mousedown', function (event) {
-  // Предотвращаем начало выделения текста при клике
-  event.preventDefault();
-});
-
-
-phoneInput.addEventListener('keydown', function (event) {
-  if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
-    event.preventDefault();
-  }
-});
-
-phoneInput.addEventListener('click', function () {
-  placeCursorAtEnd(phoneInput);
-});
-
-function placeCursorAtEnd(input) {
-  input.focus(); // Устанавливаем фокус на поле ввода
-  const length = input.value.length; // Получаем длину значения в поле
-  input.setSelectionRange(length, length); // Устанавливаем курсор в конец строки
-}
-
-let lastValue = ''; // Сохраняем последнее введенное значение
-
-function addPrefix() {
-  if (!phoneInput.value.startsWith('+38 (')) {
-    phoneInput.value = '+38 (';
-    phoneInput.setSelectionRange(6, 6); // Помещаем курсор после открывающей скобки
-  }
-}
-
-function formatePhone(event) {
-  const phoneLength = phoneInput.value.length;
-  let phoneValue = phoneInput.value;
-  console.log(phoneLength);
-
-  phoneValue = phoneValue.replace(/[^0-9+ ()]/g, '');
-
-  if (phoneLength === 9 && event.key !== ' ') {
-    phoneValue = phoneValue.slice(0, 9) + ' ' + phoneValue.slice(9);
-  }
-
-
-  if (event.key === 'Backspace' && phoneLength === 10) {
-    phoneValue = phoneValue.slice(0, -2);
-  }
-  if (event.key === 'Backspace' && (phoneLength === 14 || phoneLength === 17)) {
-    phoneValue = phoneValue.slice(0, -1);
-  }
-  if (phoneLength === 5) {
-    if (event.key !== '0') {
-      event.preventDefault();
-    }
-  }
-  if (phoneLength === 8) {
-    phoneValue += ') ';
-  }
-  if (phoneLength === 13) {
-    phoneValue += ' ';
-  }
-  if (phoneLength === 16) {
-    phoneValue += ' ';
-  }
-
-
-  if (phoneLength > 19) {
-    phoneValue = phoneValue.slice(0, 19);
-  }
-
-
-  phoneInput.value = phoneValue;
-}
-
-
-
-function preventRemovalOfPrefix(event) {
-  const phoneValue = phoneInput.value;
-
-  const prefix = '+38 (';
-
-  if (!phoneValue.startsWith(prefix)) {
-    phoneInput.value = prefix;
-  }
-}
-
-phoneInput.addEventListener('input', preventRemovalOfPrefix);
-
-
-
-phoneInput.addEventListener('keydown', formatePhone);
-
-phoneInput.addEventListener('input', function (event) {
-
-  formatePhone(event); // Передаем объект события event
-  if (phoneInput.value.length < lastValue.length) {
-    // Пользователь удалил символ, не обновляем форматирование
-    lastValue = phoneInput.value;
-    return;
-  }
-
-  checkFields();
-});
-
-phoneInput.addEventListener('focus', addPrefix);
-
-function checkFields() {
-  const nameValue = nameInput.value.trim();
-  const phoneValue = phoneInput.value.trim();
-
-  const noEnglish = /^[а-яА-ЯёЁіІїЇґҐ\s'-]+$/u.test(nameValue);
-  const nameLength = /^.{3,15}$/.test(nameValue)
-  const phoneValid = /^\+38 \(\d{3}\) \d{3} \d{2} \d{2}$/.test(phoneValue);
-
-
-
-
-  if (!nameValue) {
-    showErrorMessage(nameInput, nameErrorEmpty);
-  }
-  else {
-    hideErrorMessage(nameInput, nameErrorEmpty);
-  }
-
-  if (!noEnglish && nameValue) {
-    showErrorMessage(nameInput, nameError);
-  }
-  else {
-    hideErrorMessage(nameInput, nameError);
-  }
-  if (!nameLength && nameValue && noEnglish) {
-    showErrorMessage(nameInput, nameErrorlength);
-  }
-  else {
-    hideErrorMessage(nameInput, nameErrorlength);
-  }
-
-  if (!phoneValue) {
-    showErrorMessage(phoneInput, phoneErrorEmpty);
-  }
-  else {
-    hideErrorMessage(phoneInput, phoneErrorEmpty);
-  }
-  if (!phoneValid) {
-    showErrorMessage(phoneInput, phoneError);
-  } else {
-    hideErrorMessage(phoneInput, phoneError);
-  }
-
-  if (!nameValue || !phoneValue || !nameValid || !phoneValid) {
-    submitButton.setAttribute('disabled', 'disabled');
-  } else {
-    submitButton.removeAttribute('disabled');
-  }
-}
-
-nameInput.addEventListener('input', checkFields);
-
-
-
-const selectElement = document.getElementById('Select');
-const choices = new Choices(selectElement, {
-  allowHTML: true,
-  searchEnabled: false,
-  itemSelectText: '',
-});
