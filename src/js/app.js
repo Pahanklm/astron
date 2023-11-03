d960d7b60ce0476231962866fb0a5a3cc4793878
Pavelklm <pahanklm01@gmail.com>
2023-11-03 13:44:32 +0200

    test

--- src/js/app.js
+++ src/js/app.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			spinner.style.display = 'flex';
 			swiperNavigation.style.display = 'none';
 
-			const response = await axios.get('astron-server-production-aaac.up.railway.app');
+			const response = await axios.get('https://astron-server.onrender.com');
 			const data = response.data;
 			const data1 = data.data1;
 			const data2 = data.data2;
